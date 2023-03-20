dbf5b33f9d02f553213069de25376f1c29cea76f
Abdullah Patel <abdulpatel498@gmail.com>
2023-03-20 21:42:01 +0300

    Day2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Codepor";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Catelog App")),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
