86e89d8f70bd4932f95207acb599812c6c493df3
Abdullah Patel <abdulpatel498@gmail.com>
2023-03-26 11:26:40 +0300

    day14 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:flutter_catalog/models/catalog.dart';
-import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
-import '../widgets/item_widget.dart';
+import '../widgets/home_widgets/catelog_haeder.dart';
+import '../widgets/home_widgets/catelog_list.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -49,101 +49,12 @@ class _HomePageState extends State<HomePage> {
               children: [
                 CatelogHeader(),
                 if (CatelogModel.items.isNotEmpty)
-                  CatelogList().expand()
+                  CatelogList().py16().expand()
                 else
-                  Center(
-                    child: CircularProgressIndicator(),
-                  )
+                  CircularProgressIndicator().centered().expand(),
               ],
             ),
           ),
         ));
-  }
-}
-
-class CatelogHeader extends StatelessWidget {
-  const CatelogHeader({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        "Catelog App".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
-        "Trending Products".text.xl2.make()
-      ],
-    );
-  }
-}
-
-class CatelogList extends StatelessWidget {
-  const CatelogList({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: CatelogModel.items.length,
-      itemBuilder: (context, index) {
-        final catelog = CatelogModel.items[index];
-        return CatelogItem(catelog: catelog);
-      },
-    );
-  }
-}
-
-class CatelogItem extends StatelessWidget {
-  final Item catelog;
-
-  const CatelogItem({super.key, required this.catelog});
-  @override
-  Widget build(BuildContext context) {
-    return VxBox(
-      child: Row(
-        children: [
-          CatelogImage(image: catelog.image),
-          Expanded(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              catelog.name.text.lg.color(MyTheme.darkBluishColor).bold.make(),
-              catelog.desc.text.textStyle(context.captionStyle).make(),
-              10.heightBox,
-              ButtonBar(
-                alignment: MainAxisAlignment.spaceBetween,
-                buttonPadding: EdgeInsets.zero,
-                children: [
-                  "\$${catelog.price}".text.bold.xl.make(),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(MyTheme.darkBluishColor),
-                        shape: MaterialStateProperty.all(
-                          StadiumBorder(),
-                        )),
-                    child: "Buy".text.make(),
-                  )
-                ],
-              ).pOnly(right: 8.0)
-            ],
-          ))
-        ],
-      ),
-    ).white.rounded.square(150).make().py16();
-  }
-}
-
-class CatelogImage extends StatelessWidget {
-  final String image;
-
-  const CatelogImage({super.key, required this.image});
-
-  @override
-  Widget build(BuildContext context) {
-    return Image.network(
-      image,
-    ).box.rounded.p8.color(MyTheme.creamColor).make().p16().w40(context);
   }
 }
---
+++ lib/widgets/home_widgets/catelog_haeder.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:velocity_x/velocity_x.dart';
+import '../themes.dart';
+
+class CatelogHeader extends StatelessWidget {
+  const CatelogHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catelog App".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+        "Trending Products".text.xl2.make()
+      ],
+    );
+  }
+}
